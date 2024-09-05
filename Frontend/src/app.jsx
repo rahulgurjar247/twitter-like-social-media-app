3faf6d052379d7b45de63071e46645662596fcf6
rahul <rahulgurjar824582@gmail.com>
2024-09-05 16:36:27 +0530

    frontend setup

---
+++ Frontend/src/app.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./navbar/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/navbar/logo.jsx
@@ -0,0 +1,10 @@
+const Logo = () => {
+    return (
+        <>
+            <div className=" text-green-500 font-semibold text-2xl logo"> {"<"}<span className="text-white">Pass</span> 
+                {"247/>"} </div>
+        </>
+    ) 
+}
+
+export default Logo
