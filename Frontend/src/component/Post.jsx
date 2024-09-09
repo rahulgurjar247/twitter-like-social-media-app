1f7cfe16a4ef0a3e4ce228a1fe025bb11cc117d6
rahul <rahulgurjar824582@gmail.com>
2024-09-09 18:29:10 +0530

    design post component

---
+++ Frontend/src/component/Post.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import dev from "../assets/developer.png";
+const Post = () => {
+  return (
+    <div className="w-full  border-b flex gap-2 p-4 text-gray-600 text-left">
+      <div>
+        <img src={dev} alt="" className="w-16 rounded-xl" />
+      </div>
+      <div>
+        <div className="flex gap-1">
+          <div className="user font-semibold">Rahulgurjar</div>
+          <div className="username">@rahulgurjar247</div>
+        </div>
+        <div className="postdata ">
+          okay , i don't do this a lot but i really really wanted to post this
+          video once you've seen it do give @hello a tryDropping link bilow{" "}
+          <br /> <br /> GraphQL proves to be essential in the following
+          scenarios: Reducing app bandwidth usage as we can query multiple
+          resources in a single query. Rapid prototyping for complex systems.
+          When we are working with a graph-like data model.
+        </div>
+        <div className="panel flex justify-between mt-3">
+          <div>8</div>
+          <div>9</div>
+          <div>10</div>
+          <div>11</div>
+          <div>12</div>
+          <div>13</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
