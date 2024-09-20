a91e61cf82b5252ebfdabc1267f2e88264e5c127
rahul <rahulgurjar824582@gmail.com>
2024-09-20 19:23:12 +0530

    remove some content and import sidebar in home page

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios, { all } from "axios";
 import Post from "../component/Post";
 import PostEditor from "../component/postEditor";
+import Sidebar from "../component/Sidebar";
 
 const Home = () => {
   const [postdata, setPostData] = useState(null);
@@ -26,29 +27,11 @@ const Home = () => {
   console.log(postdata);
 
   return (
-    <div className="flex flex-row h-[100vh] w-[calc(100vw-4px)]  ">
-      <div className="w-[20%] text-center pt-10 p-2 flex flex-col gap-6  bg-slate-300 overflow-hidden font-semibold fixed h-[100vh] cursor-default">
-        <div className="">Logo</div>
-        <div className="">Home</div>
-        <div className="">Explore</div>
-        <div className="">Notification</div>
-        <div className="">Message</div>
-        <div className="">Grok</div>
-        <div className="">Profile</div>
-        <div className="">More</div>
-        <div className="bg-blue-800  rounded-2xl p-1"> Post </div>
-        <div className="overflow-hidden ">
-          <img src="" alt="" />
-          <div>
-            <h4 className="font-semibold">Rahul gurjar</h4>
-            <h3>@username</h3>
-          </div>
-        </div>
-      </div>
-
+    <div className="flex flex-row min-h-screen w-[calc(100vw)]    ">
+      <Sidebar />
       <div
-        className="w-[40%] text-center ml-[calc(20%)]
-         max-md:w-[calc(100%-20%)] z-10 overflow-y-scroll"
+        className="w-[calc(45%)] text-center ml-[calc(25%-2px)]  
+         max-md:w-[calc(100%-20%)] z-10  border-l border-r"
       >
         <PostEditor />
         {postdata &&
@@ -58,7 +41,7 @@ const Home = () => {
         <Post />
       </div>
 
-      <div className="bg-slate-50 inline-block fixed ml-[calc(100%-40%)]  text-center w-[calc(100%-60%)] max-md:hidden z-0">
+      <div className="inline-block fixed ml-[calc(100%-40%)]  text-center w-[calc(100%-60%)] max-md:hidden z-0">
         {" "}
         Side area to show trending post
       </div>
