fd7c29ebb879b95d73c3bf6779bdc95771a55875
rahul <rahulgurjar824582@gmail.com>
2024-09-20 19:24:50 +0530

    update post editor add handle post submit method

--- Frontend/src/component/postEditor.jsx
+++ Frontend/src/component/postEditor.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useRef, useState } from "react";
 import { debounce } from "lodash";
 import ProfileImgPost from "./ProfileImgPost";
+import axios from "axios";
 
 const PostEditor = () => {
   const [post, setPost] = useState({
@@ -8,25 +9,52 @@ const PostEditor = () => {
     postImage: "",
   });
   const Message = useRef();
-  const handlePost = useCallback(
-    debounce((value, name) => {
-      setPost((prevPost) => {
-        const updatedPost = {
-          ...prevPost,
-          postdata: value,
-        };
-        console.log("hyy");
-        console.log(post);
-      });
-    }, 500),
-    []
-  );
+
+  const handlepostSubmit = async (e) => {
+    console.log("post submit call");
+    e.preventDefault();
+    try {
+      if (validatePost()) {
+        console.log(post.postdata);
+        const response = await axios.post(
+          "http://localhost:3000/posts/new",
+          post,
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log(response);
+        Message.current.value = "";
+        alert("sent successfully");
+      } else {
+        throw "validation failed";
+      }
+    } catch (err) {
+      console.log("error:", err.message);
+    }
+  };
+
+  function validatePost() {
+    if (post.postdata.length < 2) {
+      return false;
+    }
+    return true;
+  }
+
+  const handlePost = (e) => {
+    e.preventDefault();
+    setPost({ ...post, [e.target.name]: e.target.value });
+    // console.log(post.postdata);
+  };
   return (
-    <div className="flex w-full p-4">
-      <div className="w-7">
+    <div className="flex w-full p-4 gap-2 text-left border-b pb-0 pr-0">
+      <div className="w-10 ">
         <ProfileImgPost />
       </div>
-      <div className="w-full border-b flex flex-col text-left pl-2">
+      <div className="w-full  flex flex-col text-left ">
         <div className="w-full  border-b">
           <textarea
             name="postdata"
@@ -34,13 +62,19 @@ const PostEditor = () => {
             className="w-full h-auto outline-none resize-none overflow-hidden"
             spellCheck="false"
             ref={Message}
-            onChange={(e) => handlePost(e.target.value, e.target.name)}
+            value={post.postdata}
+            onChange={handlePost}
           ></textarea>
         </div>
         <div className="">
           <div className="p-2 flex items-center gap-2 justify-end pl-4 hover:cursor-default">
-            <div className=" rounded-3xl w-auto  px-2 bg-blue-700">img</div>
-            <div className=" rounded-3xl w-auto px-2  bg-blue-700">send</div>
+            <div className=" rounded-3xl w-auto  px-2 bg-blue-700">Img</div>
+            <div
+              className=" rounded-3xl w-auto px-2  bg-blue-700"
+              onClick={handlepostSubmit}
+            >
+              Post
+            </div>
           </div>
         </div>
       </div>
