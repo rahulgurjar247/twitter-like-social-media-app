f11bd2f23da2357b425ef84efa10dacf5d917117
rahul <rahulgurjar824582@gmail.com>
2024-09-20 19:22:07 +0530

    post component style update

--- Frontend/src/component/Post.jsx
+++ Frontend/src/component/Post.jsx
@@ -3,21 +3,37 @@ import dev from "../assets/developer.png";
 import ProfileImgPost from "./ProfileImgPost";
 const Post = ({ postdata = "hello rahul", ...other }) => {
   return (
-    <div className="w-full  border-b flex gap-2 p-4 text-gray-600 text-left">
-      <ProfileImgPost />
-      <div>
-        <div className="flex gap-1">
+    <div className="w-full border-b border-b-[#AABBC2] flex gap-2 p-4  text-left overflow-hidden ">
+      <div className="w-10">
+        <ProfileImgPost />
+      </div>
+      <div className="w-full">
+        <div className="flex gap-1 ">
           <div className="user font-semibold">Rahulgurjar</div>
           <div className="username">@rahulgurjar247</div>
         </div>
         <div className="postdata ">{postdata.substring(0, 150)}</div>
-        <div className="panel flex justify-between mt-3">
-          <div>8</div>
-          <div>9</div>
-          <div>10</div>
-          <div>11</div>
-          <div>12</div>
-          <div>13</div>
+        <div className="panel flex justify-between mt-3 items-center [&>div>div]:hover:cursor-pointer">
+          <div className="flex flex-col items-center">
+            <div className="text-red-800 text-3xl"> &#9829;</div>
+            <div>50</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="text-red-800 text-3xl"> &#9828;</div>
+            <div>50</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="text-red-800 text-3xl"> &#9827;</div>
+            <div>50</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="text-red-800 text-3xl"> &#9825;</div>
+            <div>50</div>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="text-red-800 text-3xl"> &#9824;</div>
+            <div>50</div>
+          </div>
         </div>
       </div>
     </div>
