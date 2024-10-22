5cf56e098785a1a2dd009b3c94aff40ae30ac3b5
rahul <rahulgurjar824582@gmail.com>
2024-10-22 20:09:28 +0530

    add feature -> user can add post to post editor

--- Frontend/src/component/postEditor.jsx
+++ Frontend/src/component/postEditor.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 import { debounce } from "lodash";
 import ProfileImgPost from "./ProfileImgPost";
 import axios from "axios";
+import SelectImg from "../assets/imgSend.png";
 
 const PostEditor = () => {
   const [post, setPost] = useState({
@@ -9,6 +10,7 @@ const PostEditor = () => {
     postImage: "",
   });
   const Message = useRef();
+  const [image, setImage] = useState(null);
 
   const handlepostSubmit = async (e) => {
     console.log("post submit call");
@@ -49,6 +51,22 @@ const PostEditor = () => {
     setPost({ ...post, [e.target.name]: e.target.value });
     // console.log(post.postdata);
   };
+
+  const handleImageSubmit = (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setImage(event.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const handleImageClick = () => {
+    document.getElementById("Imginput").click();
+  };
+
   return (
     <div className="flex w-full p-4 gap-2 text-left border-b pb-0 pr-0">
       <div className="w-10 ">
@@ -59,17 +77,40 @@ const PostEditor = () => {
           <textarea
             name="postdata"
             id="writeMessage"
-            className="w-full h-auto outline-none resize-none overflow-hidden font-mono"
+            className="w-full outline-none resize-none font-mono focus:outline-none"
             spellCheck="false"
             ref={Message}
             value={post.postdata}
             onChange={handlePost}
             placeholder="What's happening?"
           ></textarea>
+          <div>
+            {image && (
+              <img
+                src={image}
+                className="w-3/5 h-40 rounded-lg overflow-hidden mb-2"
+                alt=""
+              />
+            )}
+          </div>
         </div>
         <div className="">
           <div className="p-2 flex items-center gap-2 justify-end pl-4 hover:cursor-default">
-            <div className=" rounded-3xl w-auto  px-2 bg-blue-700">Img</div>
+            <div className=" rounded-3xl w-auto  px-2 ">
+              <input
+                type="file"
+                name="Imginput"
+                id="Imginput"
+                style={{ display: "none" }}
+                accept="image/*"
+                onChange={handleImageSubmit}
+              />
+              <img
+                src={SelectImg}
+                alt="CLick to upload image"
+                onClick={handleImageClick}
+              />
+            </div>
             <div
               className=" rounded-3xl w-auto px-2  bg-blue-700"
               onClick={handlepostSubmit}
